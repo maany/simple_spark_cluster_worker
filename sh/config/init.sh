3b8fa668002b678dd97f1d9d49c2164983188ff5
maany <imptodefeat@gmail.com>
2019-04-11 16:18:51 +0200

    update init.sh

--- sh/config/init.sh
+++ sh/config/init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+echo "Copy Hadoop conf files to /root/hadoop/etc/hadoop/"
+cp /etc/simple_grid/conf/hadoop-env.sh /root/hadoop/etc/hadoop/
+cp /etc/simple_grid/conf/core-site.xml /root/hadoop/etc/hadoop/
+cp /etc/simple_grid/conf/hdfs-site.xml /root/hadoop/etc/hadoop/
+cp /etc/simple_grid/conf/mapred-site.xml /root/hadoop/etc/hadoop/
+cp -f /etc/simple_grid/conf/yarn-site.xml /root/hadoop/etc/hadoop/
+cp -f /etc/simple_grid/conf/slaves /root/hadoop/etc/hadoop/
+cat hadoop.env >> ~/.bashrc
+cat hadoop-env.sh >> ~/.bashrc
+echo "All Done!"
 
-echo "Hello"
