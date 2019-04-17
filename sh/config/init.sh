056458ef19268a27ecb5914d168de1cc3206c4ce
maany <imptodefeat@gmail.com>
2019-04-17 17:24:10 +0200

    update init.sh

--- sh/config/init.sh
+++ sh/config/init.sh
@@ -6,6 +6,7 @@ cp /etc/simple_grid/config/hdfs-site.xml /root/hadoop/etc/hadoop/
 cp /etc/simple_grid/config/mapred-site.xml /root/hadoop/etc/hadoop/
 cp -f /etc/simple_grid/config/yarn-site.xml /root/hadoop/etc/hadoop/
 cp -f /etc/simple_grid/config/slaves /root/hadoop/etc/hadoop/
+cp -f /etc/simple_grid/config/spark-defaults.cong /root/hadoop/spark/conf/
 cp -f /etc/simple_grid/config/authorized_keys ~/.ssh/authorized_keys
 cat /etc/simple_grid/config/hadoop.env >> ~/.bashrc
 cat /etc/simple_grid/config/hadoop-env.sh >> ~/.bashrc
